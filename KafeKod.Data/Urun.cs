9173fb35ace0114c69ee6de0c6c4a6a40ffe8f89
kaan.yurdakul <kaan.yurdakul@KZL-YZLMORT-B11>
2020-03-11 17:42:44 +0300

    Add project files.

---
+++ KafeKod.Data/Urun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+    public class Urun : IComparable
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return UrunAd.CompareTo(((Urun)obj).UrunAd);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1:0.00}₺", UrunAd, BirimFiyat);
+        }
+    }
+}

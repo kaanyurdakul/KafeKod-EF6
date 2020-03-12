9a3a1dc83ba5aa0f9014b744d899b878b32709bf
kaan.yurdakul <kaan.yurdakul@KZL-YZLMORT-B11>
2020-03-12 18:30:07 +0300

    Dersteki son hali.

---
+++ KafeKod/AyarlarForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace KafeKod
+{
+    partial class AyarlarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudMasaAdet = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMasaAdet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudMasaAdet
+            // 
+            this.nudMasaAdet.Location = new System.Drawing.Point(154, 22);
+            this.nudMasaAdet.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.nudMasaAdet.Name = "nudMasaAdet";
+            this.nudMasaAdet.Size = new System.Drawing.Size(135, 29);
+            this.nudMasaAdet.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Masa Adedi:";
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(57, 122);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(97, 39);
+            this.btnIptal.TabIndex = 2;
+            this.btnIptal.Text = "İptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(160, 122);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(97, 39);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // AyarlarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 190);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnIptal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudMasaAdet);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "AyarlarForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Ayarlar";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMasaAdet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudMasaAdet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
--- KafeKod/Form1.cs
+++ KafeKod/Form1.cs
@@ -33,6 +33,8 @@ namespace KafeKod
             lvwMasalar.LargeImageList = il;
             #endregion
 
+            lvwMasalar.Items.Clear();
+
             ListViewItem lvi;
             for (int i = 1; i <= Properties.Settings.Default.MasaAdet; i++)
             {
@@ -140,6 +142,19 @@ namespace KafeKod
                 }
             }
             return null;
+        }
+
+        private void tsmiAyarlar_Click(object sender, EventArgs e)
+        {
+            AyarlarForm frm = new AyarlarForm();
+
+            DialogResult dr = frm.ShowDialog();
+
+            if(dr == DialogResult.OK)
+            {
+             
+                MasalariOlustur();
+            }
         }
     }
 }
